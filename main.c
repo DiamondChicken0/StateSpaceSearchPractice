1dd768e70f910f98052dd6128a6da8973508ea41
Simon <47757232+DiamondChicken0@users.noreply.github.com>
2025-05-08 20:00:52 +0000

    Have to go to work

--- main.c
+++ main.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define N 4
+#define NxN 16
+
+
+typedef struct node 
+{
+    int tiles[N][N];
+    int depth;
+    struct node *parent;
+    struct node *next;
+};
+
+struct node *open;
+
+void printANode(struct node inp)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%i", inp.tiles[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main(int argc, char **argv)
+{
+    if (argc != NxN + 1)
+    {
+        printf("Invalid amount of arguements provided, please provide %i entries. %i provided", NxN, argc);
+        return 100;
+    }
+    struct node init;
+    for (int i = 0; i < NxN; i++)
+    {
+        init.tiles[i/N][i%N] = atoi(argv[i + 1]);
+    }
+
+    printANode(init);
+}
